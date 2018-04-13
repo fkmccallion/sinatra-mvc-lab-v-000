ae0b040f9c784f4d9785ceba701a3a72012dfacc
Frank McCallion <fkmccallion@gmail.com>
2018-04-13 11:06:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,8 +11,8 @@ class PigLatinizer
     text_arr = text.split(" ")
         result = []
         text_arr.each do |word|
-          binding.pry
-          word_arr = word.scan(/\w/)
+          
+          word_arr = word.scan(/\w/) # converts word into an array of characters
           if word_arr.first.scan(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]+/).size > 0
             c_str = word.scan(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]+/).first
             word_str = word.sub!(c_str, "")
